9fe277b2543f255d94966eef9daede1cccbffd92
Electornic <jun.lee@mvlchain.io>
2024-01-22 14:09:51 +0900

    image: add test image

--- app/page.tsx
+++ app/page.tsx
@@ -89,6 +89,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        <Image alt={''} src={'/social_impact_1.png'} width={100} height={100} />
       </div>
     </main>
   );
